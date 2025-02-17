930ff3a8c01d7e306f5f191f27cffb38e680ed34
camoloqlo <cea.rivera15@gmail.com>
2025-02-16 22:48:18 -0300

    Update FXML files, add Maven Shade Plugin, and refactor controllers

--- wos-hmi/src/main/java/cl/camodev/wosbot/city/view/CityEventsLayoutController.java
+++ wos-hmi/src/main/java/cl/camodev/wosbot/city/view/CityEventsLayoutController.java
@@ -7,6 +7,7 @@ import cl.camodev.wosbot.profile.model.IProfileObserverInjectable;
 import cl.camodev.wosbot.profile.model.ProfileAux;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class CityEventsLayoutController implements IProfileLoadListener, IProfileObserverInjectable {
 
@@ -31,6 +32,9 @@ public class CityEventsLayoutController implements IProfileLoadListener, IProfil
 	@FXML
 	private CheckBox checkBoxDailyVipRewards;
 
+	@FXML
+	private TextField textfieldExplorationOffset;
+
 	private IProfileChangeObserver profileObserver;
 
 	private boolean isLoadingProfile = false; // Bandera para controlar eventos durante la carga
@@ -42,47 +46,16 @@ public class CityEventsLayoutController implements IProfileLoadListener, IProfil
 	}
 
 	private void initializeChangeEvents() {
-		checkBoxCrystalLabFC.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_CRYSTAL_LAB_FC, newVal);
-			}
-		});
 
-		checkBoxExplorationChest.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_EXPLORATION_CHEST, newVal);
-			}
-		});
+		setupTextFieldUpdateOnFocusOrEnter(textfieldExplorationOffset, EnumConfigurationKey.INT_EXPLORATION_CHEST_OFFSET);
 
-		checkBoxHeroRecruitment.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_HERO_RECRUITMENT, newVal);
-			}
-		});
-
-		checkBoxNomadicMerchant.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_NOMADIC_MERCHANT, newVal);
-			}
-		});
-
-		checkBoxNomadicMerchantVip.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_NOMADIC_MERCHANT_VIP_POINTS, newVal);
-			}
-		});
-
-		checkBoxWarAcademyShards.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_WAR_ACADEMY_SHARDS, newVal);
-			}
-		});
-
-		checkBoxDailyVipRewards.selectedProperty().addListener((obs, oldVal, newVal) -> {
-			if (!isLoadingProfile) {
-				profileObserver.notifyProfileChange(EnumConfigurationKey.BOOL_VIP_POINTS, newVal);
-			}
-		});
+		setupCheckBoxListener(checkBoxCrystalLabFC, EnumConfigurationKey.BOOL_CRYSTAL_LAB_FC);
+		setupCheckBoxListener(checkBoxExplorationChest, EnumConfigurationKey.BOOL_EXPLORATION_CHEST);
+		setupCheckBoxListener(checkBoxHeroRecruitment, EnumConfigurationKey.BOOL_HERO_RECRUITMENT);
+		setupCheckBoxListener(checkBoxNomadicMerchant, EnumConfigurationKey.BOOL_NOMADIC_MERCHANT);
+		setupCheckBoxListener(checkBoxNomadicMerchantVip, EnumConfigurationKey.BOOL_NOMADIC_MERCHANT_VIP_POINTS);
+		setupCheckBoxListener(checkBoxWarAcademyShards, EnumConfigurationKey.BOOL_WAR_ACADEMY_SHARDS);
+		setupCheckBoxListener(checkBoxDailyVipRewards, EnumConfigurationKey.BOOL_VIP_POINTS);
 	}
 
 	@Override
@@ -90,13 +63,16 @@ public class CityEventsLayoutController implements IProfileLoadListener, IProfil
 		isLoadingProfile = true; // Deshabilita temporalmente los eventos
 
 		try {
-			checkBoxCrystalLabFC.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_CRYSTAL_LAB_FC, Boolean.class));
-			checkBoxExplorationChest.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_EXPLORATION_CHEST, Boolean.class));
-			checkBoxHeroRecruitment.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_HERO_RECRUITMENT, Boolean.class));
-			checkBoxNomadicMerchant.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_NOMADIC_MERCHANT, Boolean.class));
-			checkBoxNomadicMerchantVip.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_NOMADIC_MERCHANT_VIP_POINTS, Boolean.class));
-			checkBoxWarAcademyShards.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_WAR_ACADEMY_SHARDS, Boolean.class));
-			checkBoxDailyVipRewards.setSelected(profile.getConfig(EnumConfigurationKey.BOOL_VIP_POINTS, Boolean.class));
+			setCheckBoxValue(checkBoxCrystalLabFC, profile, EnumConfigurationKey.BOOL_CRYSTAL_LAB_FC);
+			setCheckBoxValue(checkBoxExplorationChest, profile, EnumConfigurationKey.BOOL_EXPLORATION_CHEST);
+			setCheckBoxValue(checkBoxHeroRecruitment, profile, EnumConfigurationKey.BOOL_HERO_RECRUITMENT);
+			setCheckBoxValue(checkBoxNomadicMerchant, profile, EnumConfigurationKey.BOOL_NOMADIC_MERCHANT);
+			setCheckBoxValue(checkBoxNomadicMerchantVip, profile, EnumConfigurationKey.BOOL_NOMADIC_MERCHANT_VIP_POINTS);
+			setCheckBoxValue(checkBoxWarAcademyShards, profile, EnumConfigurationKey.BOOL_WAR_ACADEMY_SHARDS);
+			setCheckBoxValue(checkBoxDailyVipRewards, profile, EnumConfigurationKey.BOOL_VIP_POINTS);
+
+			setTextFieldValue(textfieldExplorationOffset, profile, EnumConfigurationKey.INT_EXPLORATION_CHEST_OFFSET);
+
 		} finally {
 			isLoadingProfile = false; // Reactiva los eventos después de cargar el perfil
 		}
@@ -106,6 +82,59 @@ public class CityEventsLayoutController implements IProfileLoadListener, IProfil
 	public void setProfileObserver(IProfileChangeObserver observer) {
 		this.profileObserver = observer;
 
+	}
+
+	private void setupCheckBoxListener(CheckBox checkBox, EnumConfigurationKey configKey) {
+		checkBox.selectedProperty().addListener((obs, oldVal, newVal) -> {
+			if (!isLoadingProfile) {
+				profileObserver.notifyProfileChange(configKey, newVal);
+			}
+		});
+	}
+
+	private void updateProfile(TextField textField, EnumConfigurationKey configKey) {
+		String newVal = textField.getText();
+		if (isValidPositiveInteger(newVal)) {
+			profileObserver.notifyProfileChange(configKey, Integer.valueOf(newVal));
+		} else {
+			textField.setText(configKey.getDefaultValue());
+		}
+	}
+
+	private void setupTextFieldUpdateOnFocusOrEnter(TextField textField, EnumConfigurationKey configKey) {
+		textField.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+			if (!isNowFocused && !isLoadingProfile) {
+				updateProfile(textField, configKey);
+			}
+		});
+
+		textField.setOnAction(event -> {
+			if (!isLoadingProfile) {
+				updateProfile(textField, configKey);
+			}
+		});
+	}
+
+	private boolean isValidPositiveInteger(String value) {
+		if (value == null || value.isEmpty()) {
+			return false;
+		}
+		try {
+			int number = Integer.parseInt(value);
+			return number >= 0 && number <= 24;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	private void setCheckBoxValue(CheckBox checkBox, ProfileAux profile, EnumConfigurationKey key) {
+		Boolean value = profile.getConfig(key, Boolean.class);
+		checkBox.setSelected(value != null && value);
+	}
+
+	private void setTextFieldValue(TextField textField, ProfileAux profile, EnumConfigurationKey key) {
+		Integer value = profile.getConfig(key, Integer.class);
+		textField.setText(value != null ? String.valueOf(value) : key.getDefaultValue());
 	}
 
 }
