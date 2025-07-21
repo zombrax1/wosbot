369f007c136b8c9e79a0fa1301282492c2ee7daf
zombrax1 <50038184+zombrax1@users.noreply.github.com>
2025-07-21 09:32:42 +0800

    fix: import EnumTpMessageSeverity

--- wos-serv/src/main/java/cl/camodev/wosbot/serv/task/impl/HeroRecruitmentTask.java
+++ wos-serv/src/main/java/cl/camodev/wosbot/serv/task/impl/HeroRecruitmentTask.java
@@ -2,6 +2,7 @@ package cl.camodev.wosbot.serv.task.impl;
 
 import cl.camodev.wosbot.console.enumerable.EnumTemplates;
 import cl.camodev.wosbot.console.enumerable.TpDailyTaskEnum;
+import cl.camodev.wosbot.console.enumerable.EnumTpMessageSeverity;
 import cl.camodev.wosbot.ot.DTOImageSearchResult;
 import cl.camodev.wosbot.ot.DTOPoint;
 import cl.camodev.wosbot.ot.DTOProfiles;
